ce74607c3f120791b25715987cb8bff97576aea8
José Neto <joseneto002@gmail.com>
2022-09-29 14:23:37 -0300

    Client, banco H2

---
+++ src/test/java/com/joseneto/CRUDClients/CrudClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseneto.CRUDClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
